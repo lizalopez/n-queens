6628ec5e6c4c4beb826b4fe0baaa1e2c36182790
lizalopez <liza.b.lopez@gmail.com>
2015-12-02 20:52:30 -0800

    Debugging rook combo

--- src/solvers.js
+++ src/solvers.js
@@ -11,36 +11,52 @@
 // take a look at solversSpec.js to see what the tests are expecting
 
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, 
-// with n rooks placed such that none of them can attack each other
+// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
 
 window.findNRooksSolution = function(n) {
-  var solution = [];
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  // rowArr.length = n;
-  for (var ri = 0; ri < n; ri++) {
-    
-    var colArr = [];
-    for (var ci = 0; ci < n; ci++) {
-      if (ri === 0 && ci === 0) {
-        colArr = colArr.concat(1);
-      } else {
-        colArr = colArr.concat(0);
-      }
-    }
-
-    solution.push(colArr);
+  var solution = new Board({n : n});
+  for (var i = 0; i < n; i ++) {
+    solution.togglePiece(i, i); 
   }
-  console.log('sol: ', solution);
-  return solution;
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var solutionCount = 0;
 
+  function subroutine(rounds, combo) {
+    combo = combo || new Board({n : n});
+
+    if (rounds === 0) {
+      // solution.push( combo.rows() );
+      solutionCount++;
+      return;
+    }
+
+    for (var ir = 0; ir < n; ir++) {
+      for (var ic = 0; ic < n; ic ++) {
+        if ( !combo.hasRowConflictAt(ir) && !combo.hasColConflictAt(ic) ) {
+          combo.togglePiece(ir, ic);
+          subroutine( rounds - 1, combo );
+        }
+      }
+    }
+  }
+  //base case - n === 0 (for this round) 
+    // push combination
+
+  //iterate through n rows, n columns
+    //if no conflict
+      // recurse(n - 1, toggle rook concat)
+  //subroutine(n)
+  subroutine(n);
+  debugger;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
